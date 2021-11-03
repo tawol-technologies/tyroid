02dfd4331bf079533f627a2a7155685455c86caf
kaynetpc <89577665+kakinwumi@users.noreply.github.com>
2021-11-03 15:15:29 +0100

    Update index.ts

--- array/index.ts
+++ array/index.ts
@@ -1,1 +1,29 @@
-export const arrays = {};
+
+/**
+ * 
+ * @param data numbers
+ * @returns 
+ */
+const  addAll = (data: Array<any>) => {
+    let res = 0;
+    for (let k = 0; k < data.length; k++) {
+      res += parseFloat(data[k]);
+    }
+    return res;
+  }
+
+/**
+ * Generate array percentage
+ */
+
+const percent = 100;
+const arrayOfNumberToPercentage = (arr: Array<number>) => {
+    return arr.map(x => x / addAll(arr) * percent);
+}
+
+
+
+
+export const arrays = {
+    arrayOfNumberToPercentage
+};
