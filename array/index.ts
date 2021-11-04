4c29a030a8f743afb1fc361b51763cf1d23c6020
hanspet9x <peterakinlolu1@gmail.com>
2021-11-04 13:12:42 +0100

    added net

--- array/index.ts
+++ array/index.ts
@@ -1,1 +1,14 @@
-export const arrays = {};
+/**
+ * It destructures all objects in the array into an object and returns it.
+ * @param objects Array<{}>
+ * @returns object
+ */
+ export const mergeObjects = (objects: {}[]):{} => {
+    let obj:{} = {};
+    objects.forEach(o => obj = {...obj, ...o});
+    return obj;
+}
+
+export const arrays = {
+    mergeObjects
+};
