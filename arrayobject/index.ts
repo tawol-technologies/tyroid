d48f891492378c3c24897a835608be7fae7ae6ee
Ayomide Oyediran <joyediran@tawol-tech.com>
2022-02-25 14:32:20 +0100

    add search function to array object

--- arrayobject/index.ts
+++ arrayobject/index.ts
@@ -12,6 +12,40 @@ export const getKeyValues = (data: Array<Record<string, unknown>>, key: string):
     return res;
 }
 
+export const charsIncluded = (data: string, chars: string[]): number => {
+    let counter: number = 0;
+    for (let i = 0; i < chars.length; i++) {
+        if(data.includes(chars[i])) {
+            counter++;
+        }
+    }
+    return counter;
+}
+
+export const search = (data: Array<Record<string, object>>, value: string): Array<Record<string, object>> => {
+    let res:Array<Record<string, object>> = [];
+    const searchChars: string[] = value.split("");
+    let elementMatchCounter: number = 0;
+    data.forEach(element => {
+        let objMatchCounter: number = 0;
+        for (const key in element) {
+            const num: number = charsIncluded(element[key]+"" , searchChars)
+            if (num > objMatchCounter) {
+                objMatchCounter = num;
+            }
+        }
+        //To make sure perfect match comes first
+        if (objMatchCounter > elementMatchCounter) {
+            elementMatchCounter = objMatchCounter;
+            res.unshift(element);
+        }
+        else if(objMatchCounter !== 0) {
+            res.push(element);
+        }
+    });
+    return res;
+}
+
 export default {
-    getKeyValues
+    getKeyValues, charsIncluded, search
 }
