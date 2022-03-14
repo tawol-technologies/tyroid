63f8722331a60472d96065edfee6ee81bfa8eef9
Ayomide Oyediran <oyediranjoysbright@gmail.com>
2022-03-14 09:56:28 +0100

    update arrayobject/index.ts

--- arrayobject/index.ts
+++ arrayobject/index.ts
@@ -28,11 +28,12 @@ export const sortObjectValuesByChars = (data: Array<Record<string, object>>, sea
     data.forEach(element => {
         let objMatchCounter: number = 0;
         for (const key in element) {
+            let num: number = 0;
            if(key.includes(searchChars.join(","))){
            num = searchChars.length;
            } else {
            
-            num: number = countCharsInData(element[key]+"" , searchChars)
+            num = countCharsInData(element[key]+"" , searchChars)
             }
             if (num > objMatchCounter) {
                 objMatchCounter = num;
